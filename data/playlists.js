18ca3e51dde9572645e77a731a90f4d4621e1451
cmmerritt <christiane.merritt@gmail.com>
2021-05-27 22:18:29 -0700

    add real playlists to seed data

--- data/playlists.js
+++ data/playlists.js
@@ -1,43 +1,43 @@
 // example seed data
 const playlists = [
   {
-    playlist_id: '1234',
-    title: 'Bangin Summer Vibes',
-    theme: 'new wave',
-    note: 'Once upon a vibin time',
-    recipient: 'dork',
+    playlist_id: '1234PLNl9oExSlC5iaCBewxo2Ogk1QSgbxDSri',
+    title: 'Silent Music Videos - LOL',
+    theme: 'Ophiuchus',
+    note: 'Music videos are better without music.',
+    recipient: 'Picard',
     user_id: 1
   },
   {
-    playlist_id: '2345',
-    title: 'Bangin Spring Vibes',
-    theme: 'next wave',
-    note: 'Once upon a spring time',
-    recipient: 'nerd',
+    playlist_id: '2345PLFEFA0B8A493CDF7D',
+    title: 'Star Trek Vibes',
+    theme: 'Ceti-Alpha-5',
+    note: 'Imagine you\'re going where no one has gone before.',
+    recipient: 'Pike',
     user_id: 2
   },
   {
-    playlist_id: '3456',
-    title: 'Bangin Winter Vibes',
-    theme: 'dream wave',
-    note: 'Once upon a winter time',
-    recipient: 'jerk',
+    playlist_id: 'PL29795DD1572DC72C',
+    title: 'Alien Vibes',
+    theme: 'Zeta-Reticuli',
+    note: 'Nobody down here but the FBI\'s most unwanted.',
+    recipient: 'Agent Scully',
     user_id: 3
   },
   {
-    playlist_id: '4567',
-    title: 'Bangin Autumn Vibes',
-    theme: 'cyberpunk',
-    note: 'Once upon a autumn time',
-    recipient: 'loser',
+    playlist_id: 'PLMyUd6oa71FX9EMr8Nr-NMRT5YOxq0Vmx',
+    title: 'The Punk Singer soundtrack',
+    theme: 'Ursa-Major',
+    note: 'You are the queen of my world',
+    recipient: 'Cool kid',
     user_id: 4
   },
   {
-    playlist_id: '5678',
-    title: 'Bangin Bangin Vibes',
-    theme: 'bang wave',
-    note: 'For whom the bang tolls',
-    recipient: 'badass',
+    playlist_id: 'PL26E7604B1A8FC8F0',
+    title: 'Change of the Century',
+    theme: 'Stardust',
+    note: 'Thought you\'d enjoy some jazz',
+    recipient: 'Ornette',
     user_id: 5
   }
 ];
